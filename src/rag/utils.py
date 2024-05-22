913b42908f135dccfd1b57d7e09271a744e85d56
unknown <dauvannam321@gmail.com>
2024-05-22 19:22:21 +0700

    update

---
+++ src/rag/utils.py
@@ -0,0 +1,13 @@
+# File code dùng để khai báo hàm tách câu trả lời từ model.
+
+import re
+
+def extract_answer(text_response: str,
+                   pattern: str = r"Answer:\s*(.*)"
+                   ) -> str:
+    match = re.search(pattern, text_response)
+    if match:
+        answer_text = match.group(1).strip()
+        return answer_text 
+    else:
+        return "Answer not found."
