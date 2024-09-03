c88e15cb4aaff25fb504da0b4c25590ca6542b9a
dauvannam1804 <dauvannam321@gmail.com>
2024-09-03 23:46:55 +0700

    modify: model_name

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
-def get_hf_llm(model_name: str = "mistralai/Mistral-7B-Instruct-v0.2",
+def get_hf_llm(model_name: str = "togethercomputer/RedPajama-INCITE-Chat-3B-v1",
                max_new_token = 1024, **kwargs):
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
